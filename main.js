ac0fa81f1f6d9df0d7df1457a87fb1aa0b80184b
abdujalil-nizomiddinov <abdujalil2107@gmail.com>
2025-03-04 22:13:58 +0500

    djbdjfd

---
+++ main.js
@@ -0,0 +1,75 @@
+// // TASK - 1
+
+// let password_1 = prompt("Foydalanuvchi parolini kiriting...");
+// let password_2 = prompt("Parolni tasdiqlang...");
+// console.log(password_1 == password_2);
+
+// // TASK - 1
+// // TASK - 2
+
+// let num = Number(prompt("Tasodifiy raqam kiriting..."));
+// console.log(num % 2 === 0);
+
+// // TASK - 2
+// // TASK - 3
+
+// let num = Number(prompt("Tasodifiy raqam kiriting..."));
+// console.log("Kvadrati:", num * num);
+// console.log("Kubi:", num * num * num);
+// console.log("Ildizi:", Math.sqrt(num));
+
+// // TASK - 3
+// // TASK - 4
+
+// let x = Number(prompt("X hajmini kiriting..."));
+// let y = Number(prompt("Y hajmini kiriting..."));
+// console.log("Yuzi:", x * y);
+// console.log("Kubi:", 2 * (x + y));
+
+// // TASK - 4
+// // TASK - 5
+
+// let num_1 = Number(prompt("Son-1 kiriting:"));
+// let num_2 = Number(prompt("Son-2 kiriting:"));
+// let num_3 = Number(prompt("Son-3 kiriting:"));
+
+// console.log("Eng katta son:", Math.max(num_1, num_2, num_3););
+// console.log("Eng kichik son:", Math.min(num_1, num_2, num_3););
+
+// // TASK - 5
+// // TASK - 6
+
+// let num_1 = Number(prompt("Son_1 kiriting:"));
+// let num_2 = Number(prompt("Son_2 kiriting:"));
+
+// console.log("Natija:", num_1 ** 3 / num_2 ** 2);
+
+// // TASK - 6
+// // TASK - 7
+
+// let usd = Number(prompt("USD to UZS kiriting-USD..."));
+
+// console.log("UZS:", usd * 12200);
+
+// // TASK - 7
+// // TASK - 8
+
+// let num_1 = Number(prompt("Son-1 kiriting:"));
+// let num_2 = Number(prompt("Son-2 kiriting:"));
+
+// console.log("Natija:", (num_2 / num_1) * 100 + "%");
+
+// // TASK - 8
+// // TASK - 9
+
+// let min = Number(prompt("Daqiqa kiriting:"));
+
+// console.log("Secund:", min * 60);
+
+// // TASK - 9
+// TASK - 10
+
+// Tepadagi 9 ta taskni bajaring keling.
+// Bajardim borsam kerak 😅
+
+// TASK - 10
